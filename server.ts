dc8ffd89d40cd7166c75bb5d67d08c7e2a7d2777
bolatah <bolatah@outlook.com>
2023-02-01 13:44:54 +0100

    Adding an env variable

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import serveFavicon = require("serve-favicon");
 import path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(serveFavicon(__dirname + "/favicon.ico"));
 app.use(express.static(path.join(__dirname)));
@@ -15,4 +16,4 @@ app.get("*", function (_req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(8080);
+app.listen(PORT);
